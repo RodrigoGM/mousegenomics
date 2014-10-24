3dc6beaad7cad83f209d4e8a1c87d487ea1e4fa0
Rodrigo Gularte Merida <rodrigo.gularte@ulg.ac.be>
2014-10-24 16:29:06 +0200

    modified:   gatk.qpp.scala 	    Changed the extension of VCF outputs to ouput compressed VCF files. Furthermore, the script runs HaplotypeCaller twice, once to generate the genomic vcf (g.vcf.gz) and a second time to generate the sample specific vcf (vcf.gz)
    modified:   gatk.qpp.sh
    	    Work changes

--- gatk.qpp.sh
+++ gatk.qpp.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 #SBATCH --time=48:00:00
 
-## kill if anything fails
-set -e
-## ensure consistency of paths, modules, etc...
-source ~/.bashrc
-## load environment variables containing Grcm38 file paths
-source $(find $GLOBALSCRATCH/tgv -name 'envar.sh' -type f)
-## path/to/GATK
-GATK=~/programs/GATK-latest/
+
+set -e                                                       ## kill if anything fails
+source ~/.bashrc                                             ## ensure consistency of paths, modules, etc...
+source $(find $GLOBALSCRATCH/tgv -name 'envar.sh' -type f)   ## load environment variables containing Grcm38 file paths
+GATK=~/programs/GATK-latest/                                 ## path/to/GATK
+
 
 # Input file at the comand line
+# for i in $(ls *dd.bam) ; do ./gatk.qpp.sh $i & done
 input=$1
 
-echo "============== In/Del Realingment + Base Recalibration  =================="
+echo "============== GATK Pipeline  =================="
+echo "============== In/Del Realignment  =================="
+echo "============== BasePair Recalibration  =================="
+echo "============== 2nd. BasePair Recalibration  =================="
+echo "============== Variant Discovery w/ Haplotype Caller  =================="
 echo `date`
 # -P = scatter parameter
 
 java -Xmx500M -jar ${GATK}/Queue.jar \
     -S gatk.qpp.scala \
-    -P 5 \
+    -P 15 \
     -R $MM10 \
     -I $input \
     -known $INDELVCF \
     -N 6 -C 6 \
     -jobRunner Drmaa \
     -jobNative "--time=12:00:00 --nodes=1 --ntasks-per-node=6 --mem-per-cpu=4000" \
-    -run -startFromScratch
+    -l DEBUG
+
+##    -run
+
+##    -startFromScratch -run
 
 echo `date`
 echo "============== Finished =================="
