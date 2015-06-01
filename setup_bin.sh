4c1731146c4f7f88e302825cb4ef28958943c675
Rodrigo Gularte Mérida <rodrigo.gularte@ulg.ac.be>
2015-06-01 14:46:11 +0200

    Update setup_bin.sh

--- setup_bin.sh
+++ setup_bin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-## creates sym links for all executable files in ~/programs with -type f and -perm +111
+## creates sym links for all executable files in ~/src with -type f and -perm +111
 ## in the $HOME/bin directory
 
-for exec in $(find $HOME/programs/* -type f -perm +111)
+for exec in $(find $HOME/src/* -type f -perm +111)
 do 
     if [ -x $exec ]
 	then cp -rs $exec ~/bin/
@@ -19,21 +19,21 @@ rm *.fa *.sample *.bed Makefile *.rst *genome *.sh *.html *.css *.fna *.ebwt AUT
 
 cd ../
 
-## creates sym links for all shell and perl scripts in $HOME/programs
+## creates sym links for all shell and perl scripts in $HOME/src
 ## in the $HOME/scripts directory
 if [ -f $HOME/scripts ]
     then echo found scripts
     else mkdir scripts
 fi
 
-for exec in $(find $HOME/programs/* -name "*.sh")
+for exec in $(find $HOME/src/* -name "*.sh")
 do 
     if [ -x $exec ]
 	then cp -rs $exec $HOME/scripts/
     fi
 done
 
-for exec in $(find $HOME/programs/* -name "*.pl")
+for exec in $(find $HOME/src/* -name "*.pl")
 do 
     if [ -x $exec ]
 	then cp -rs $exec $HOME/scripts/
